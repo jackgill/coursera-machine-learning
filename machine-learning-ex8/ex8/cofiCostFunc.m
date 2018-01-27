30204f3b912d396e1f869c43cabfc208d553fa4c
Jack Gill <jack@jackmgill.com>
2018-01-27 13:36:44 -0700

    Solutions for exercise 8 in Octave

--- machine-learning-ex8/ex8/cofiCostFunc.m
+++ machine-learning-ex8/ex8/cofiCostFunc.m
@@ -42,16 +42,26 @@ Theta_grad = zeros(size(Theta));
 
 
 
+J = (1 / 2) * sum(sum(R .* (X * Theta' - Y) .^ 2)) + ...
+    (lambda / 2) * sum(sum(Theta .^ 2)) + ...
+    (lambda / 2) * sum(sum(X .^2));
+
+for i = 1:num_movies
+  idx = find(R(i, :) == 1);
+  Theta_temp = Theta(idx, :);
+  Y_temp = Y(i, idx);
+  X_grad(i, :) = (X(i, :) * Theta_temp' - Y_temp) * Theta_temp + ...
+                 lambda * X(i, :);
+end
 
 
-
-
-
-
-
-
-
-
+for j = 1:num_users
+  idx = find(R(:, j) == 1);
+  X_temp = X(idx, :);
+  Y_temp = Y(idx, j);
+  Theta_grad(j, :) = (X_temp * Theta(j, :)' - Y_temp)' * X_temp + ...
+                     lambda * Theta(j, :);
+end
 
 
 
